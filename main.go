220dc20cb89ed6ca5aa1d6a4a5814cd664090135
Jay Boehm <jayrox@gmail.com>
2017-10-21 10:28:38 -0400

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 
 func main() {
 	var err = filepath.Walk("./", func(file string, _ os.FileInfo, _ error) error {
-		fmt.Println(file)
+		fmt.Println("__________")
+		basename := filepath.Base(file)
+		fmt.Println(basename)
+		fp := filepath.Dir(file)
+		fmt.Println(fp)
+
 		return nil
 	})
 	if err != nil {
